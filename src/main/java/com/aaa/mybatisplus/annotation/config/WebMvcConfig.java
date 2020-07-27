4c76b2480387e72f749471fec463d66031c88f9d
tianliuzhen <tlzallen@163.com>
2020-07-27 19:48:39 +0800

    完善springMvc响应 字符串乱码

--- src/main/java/com/aaa/mybatisplus/annotation/config/WebMvcConfig.java
+++ src/main/java/com/aaa/mybatisplus/annotation/config/WebMvcConfig.java
@@ -1,56 +1,54 @@
 package com.aaa.mybatisplus.annotation.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.*;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
  * 重写WebMvcConfigurationSupport后，springboot默认配置的信息会失效
- * 譬如：utf-8问题、静态资源无法问题。
- * @author liuzhen.tian
- * @version 1.0    2020/6/23 20:57
+ * 譬如：utf-8问题（controller 返回值 乱码）、静态资源无法问题。
+ * 这里配置 extends WebMvcConfigurationSupport 的作用仅是为了实现自定义的拦截器
+ * 项目中还可以通过配置 aop 的形式去修改参数值
+ *
+ * @author liuzhen.tian  time: 2020/6/23 20:57
  */
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
-    //添加自定义的拦截器
+    /**
+     * 添加自定义的拦截器
+     */
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(new ParameterInfoResolver());
     }
-
-
     /**
      * 中文乱码问题
      * @return
      */
-    @Bean
-    public HttpMessageConverter<String> responseBodyConverter() {
-        //StringHttpMessageConverter是一个请求和响应信息的编码转换器，
-        // 通过源码我们发现默认编码ISO-8859-1，不是UTF-8，所以我们只要通过上述配置将请求字符串转为UTF-8 即可
-        StringHttpMessageConverter converter = new StringHttpMessageConverter(
-                Charset.forName("UTF-8"));
-        return converter;
-    }
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        super.configureMessageConverters(converters);
-        converters.add(responseBodyConverter());
-    }
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorPathExtension(false);
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 解决controller返回字符串中文乱码问题
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof StringHttpMessageConverter) {
+                ((StringHttpMessageConverter)converter).setDefaultCharset(StandardCharsets.UTF_8);
+            }
+        }
     }
 
-
+    // ！！！不能用这种方式 设置utf-8，如果用了将不会添加其他的converter
+    // @Override
+    // public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
+    //     converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
+    // }
 
     /**
-     * 静态资源无法访问问题 eg:访问 swagger-ui.html 会出现 404
+     *  extends WebMvcConfigurationSupport 造成问题
+     *  静态资源无法访问问题 eg:访问 swagger-ui.html 会出现 404
      * @param registry
      */
     @Override
@@ -61,14 +59,8 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/doc.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
         super.addResourceHandlers(registry);
-    }
-
-    /**
-     * 配置默认servlet处理
-     */
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
     }
 }
