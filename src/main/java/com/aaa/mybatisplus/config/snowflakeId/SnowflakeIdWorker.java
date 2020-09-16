c30a07b9c131b52491879e2ca0c0e1b82e23a2e6
tianliuzhen <tlaallen@163.com>
2020-09-16 12:09:05 +0800

    完善 雪花算法组件

--- src/main/java/com/aaa/mybatisplus/config/snowflakeId/SnowflakeIdWorker.java
+++ src/main/java/com/aaa/mybatisplus/config/snowflakeId/SnowflakeIdWorker.java
@@ -1,12 +1,19 @@
-package com.aaa.mybatisplus.config;
+package com.aaa.mybatisplus.config.snowflakeId;
 
 /**
- * description: java 版雪花算法
- * 注意: 单线程 for 循环测试会 有重复Id
+ * SnowFlake简介
+ *    SnowFlake的结构如下(每部分用-分开):
+ *         0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
+ *    1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0
+ *    41位时间戳(毫秒级)，注意，41位时间戳不是存储当前时间的时间戳，而是存储时间戳的差值（当前时间戳 - 开始时间戳)
+ *    得到的值），这里的的开始时间戳，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间戳，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69
+ *    10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId
+ *    12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间戳)产生4096个ID序号
+ *    加起来刚好64位，为一个Long型。
  *
- * @author 田留振(liuzhen.tian @ haoxiaec.com)
- * @version 1.0
- * @date 2019-12-20
+ * SnowFlake的优点:
+ *    整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，
+ *    并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
  */
 public class SnowflakeIdWorker {
 
