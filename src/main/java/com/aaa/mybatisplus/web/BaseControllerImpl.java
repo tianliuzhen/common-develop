bd1f6ab8390a67d694d24a0b6b21078364780137
tianliuzhen <tlaallen@163.com>
2020-10-09 12:40:49 +0800

    add HttpContextUtils ...

--- src/main/java/com/aaa/mybatisplus/web/BaseControllerImpl.java
+++ src/main/java/com/aaa/mybatisplus/web/BaseControllerImpl.java
@@ -1,17 +1,22 @@
 package com.aaa.mybatisplus.web;
 
 import com.aaa.mybatisplus.annotation.Assignment;
+import com.aaa.mybatisplus.annotation.LessLog;
 import com.aaa.mybatisplus.annotation.SysLog;
 import com.aaa.mybatisplus.annotation.SysTimeLog;
 import com.aaa.mybatisplus.config.httpResult.type.ResultResponse;
 import com.aaa.mybatisplus.domain.dto.PageDto;
 import com.aaa.mybatisplus.domain.dto.UserDto;
+import com.aaa.mybatisplus.domain.enums.LogType;
+import com.aaa.mybatisplus.util.CommonUtils;
+import com.aaa.mybatisplus.util.HttpContextUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,9 +37,12 @@ public class BaseControllerImpl  {
 
     @PostMapping ("/testStr")
     public ResultResponse<String> testStr(@Valid @RequestBody PageDto pageDto) {
-            Map map=new HashMap();
-            map.put("key","val");
-            return  new ResultResponse<>("字符串");
+        Map map = new HashMap();
+        map.put("key", "val");
+        System.out.println(CommonUtils.getIpAddr(HttpContextUtils.getHttpServletRequest()));
+        System.out.println(HttpContextUtils.getDomain());
+        System.out.println(HttpContextUtils.getOrigin());
+        return new ResultResponse<>("字符串");
 
     }
     //不打印响应日志
