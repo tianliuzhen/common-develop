8d0dee62236df3d051ccf7c346070d6773f52db9
tianliuzhen <tlzallen@163.com>
2020-10-21 00:07:40 +0800

    CountDownLatch在SpringBoot中配合@Async使用

---
+++ src/main/java/com/aaa/mybatisplus/web/AsyncController.java
@@ -0,0 +1,26 @@
+package com.aaa.mybatisplus.web;
+
+import com.aaa.mybatisplus.annotation.SysTimeLog;
+import com.aaa.mybatisplus.domain.entity.Entity;
+import com.aaa.mybatisplus.service.AsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0 AsyncController.java  2020/10/20 23:44
+ */
+@RestController
+public class AsyncController {
+
+    @Autowired
+    private AsyncService asyncService;
+
+    @GetMapping("/testAsync")
+    public List<Entity> testAsync(){
+        return asyncService.queryData();
+    }
+}
