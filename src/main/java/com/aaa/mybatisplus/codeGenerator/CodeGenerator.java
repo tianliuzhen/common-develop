75c3ad3afb428ce663f5ad97c68d493a7099a508
tianliuzhen <tlaallen@163.com>
2020-12-07 19:11:15 +0800

    升级 pom 相关依赖版本

--- src/main/java/com/aaa/mybatisplus/codeGenerator/CodeGenerator.java
+++ src/main/java/com/aaa/mybatisplus/codeGenerator/CodeGenerator.java
@@ -2,13 +2,13 @@ package com.aaa.mybatisplus.codeGenerator;
 
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
