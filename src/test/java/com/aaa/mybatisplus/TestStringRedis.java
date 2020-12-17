03f478d8875e65aa3bcf9baf25bb319e294b6f84
tianliuzhen <tlzallen@163.com>
2020-12-17 15:28:23 +0800

    fix

--- src/test/java/com/aaa/mybatisplus/TestStringRedis.java
+++ src/test/java/com/aaa/mybatisplus/TestStringRedis.java
@@ -1,5 +1,7 @@
 package com.aaa.mybatisplus;
 
+import com.aaa.mybatisplus.domain.entity.City;
+import com.aaa.mybatisplus.domain.entity.People;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -31,10 +33,26 @@ public class TestStringRedis {
     @Test
     @SuppressWarnings("rawtypes")
     public void testString() {
+
+        // 测试set
         redisTemplate.opsForValue().set("a","b");
         redisTemplate.opsForValue().set("b","c");
         List list = redisTemplate.opsForValue().multiGet(new ArrayList(Arrays.asList("a", "b")));
         list.forEach(System.out::println);
+
+        People people = new People("tom", 11, new City(), true, new ArrayList());
+        redisTemplate.opsForValue().set("tom",people);
+        People tom = (People)redisTemplate.opsForValue().get("tom");
+        System.out.println(tom);
+
+
+        // 测试 hash
+        List map = Arrays.asList(new People("tom", 11, new City(), true, new ArrayList()),
+                new People("tom2", 12, new City(), true, new ArrayList())
+        );
+        redisTemplate.opsForHash().put("map:key","t1",map);
+        List peoples= (List) redisTemplate.opsForHash().get("map:key","t1");
+        System.out.println(peoples);
     }
 
 
