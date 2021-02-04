cf105d58fce419a76db79e3c997a239be9a9d1c3
tianliuzhen <tlzallen@163.com>
2021-02-04 22:51:13 +0800

    Groovy+Spock 写 userService 测试用例

---
+++ src/test/java/com/aaa/spock/UserServiceTestSpec.groovy
@@ -0,0 +1,41 @@
+package com.aaa.spock
+
+import com.aaa.mybatisplus.domain.entity.User
+import com.aaa.mybatisplus.mapper.UserMapper
+import com.aaa.mybatisplus.service.impl.User2ServiceImpl
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page
+import com.google.common.collect.Lists
+import spock.lang.Specification
+
+
+class UserServiceTestSpec extends Specification {
+    def userService = new User2ServiceImpl()
+
+
+    //注入mock数据
+    def setup() {
+        userService.userMapper = Mock(UserMapper)
+    }
+
+    def "测试 user"() {
+        given:
+        //准备数据
+        userService.userMapper.selectPageVo(_, _) >>
+                new Page<User>(records: Lists.asList(
+                        new User("id": 1, "name": "小米"),
+                        new User("id": 2, "name": "小三")
+                ))
+
+        expect:
+        def result= userService.selectUserPage(input1,input2)
+        result.getSize() == 10
+        result.records.size() ==2
+
+        where:
+        input1 = new Page<User>(size: 10, current: 1)
+        input2 = "小三"
+
+    }
+
+}
+
