607f9bd677afdee7161ab5f279f17a5260100fba
tianliuzhen <tlzallen@163.com>
2022-02-12 17:31:10 +0800

    注释

--- src/main/java/com/aaa/mybatisplus/util/SendMail.java
+++ src/main/java/com/aaa/mybatisplus/util/SendMail.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 
 /**
  * description: 参考：https://blog.csdn.net/xuemengrui12/article/details/78530594
+ * 常用邮箱SMTP服务器地址大全： https://blog.csdn.net/ning521513/article/details/79217203
  *
  * @author 田留振(liuzhen.tian @ haoxiaec.com)
  * @version 1.0
@@ -20,8 +21,8 @@ public class SendMail {
     // 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
     // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）,
     //     对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
-    public static String myEmailAccount = "tlzallen@126.com";
-    public static String myEmailPassword = "DTVZDSSPHMLGQNMH";
+    public static String myEmailAccount = "tlzallen@126.com"; // 邮箱密码是我的通用密码 结尾 + @
+    public static String myEmailPassword = "RVLTSROSQBJPEQUZ";
 
     // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
     // 网易126邮箱的 SMTP 服务器地址为: smtp.126.com
