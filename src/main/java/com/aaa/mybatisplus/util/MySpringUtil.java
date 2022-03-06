d63358c55b2bef857810fe4c30d99d1fa46084f7
tianliuzhen <tlzallen@163.com>
2022-03-06 21:15:04 +0800

    规范代码

--- src/main/java/com/aaa/mybatisplus/util/MySpringUtil.java
+++ src/main/java/com/aaa/mybatisplus/util/MySpringUtil.java
@@ -51,7 +51,9 @@ public class MySpringUtil implements ApplicationContextAware {
         Set<String> emptyNames = new HashSet<String>();
         for(java.beans.PropertyDescriptor pd : pds) {
             Object srcValue = src.getPropertyValue(pd.getName());
-            if (srcValue == null) emptyNames.add(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
         }
         String[] result = new String[emptyNames.size()];
         return emptyNames.toArray(result);
