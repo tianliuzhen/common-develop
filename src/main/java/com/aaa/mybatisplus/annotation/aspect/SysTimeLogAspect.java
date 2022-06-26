cca20c406552e043a40317fd064f9261f9a95614
tianliuzhen <tlzallen@163.com>
2022-06-26 16:14:42 +0800

    aop记录执行时间采用stopWatch工具类

--- src/main/java/com/aaa/mybatisplus/annotation/aspect/SysTimeLogAspect.java
+++ src/main/java/com/aaa/mybatisplus/annotation/aspect/SysTimeLogAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 /**
  * description: 描述
@@ -22,23 +23,29 @@ import org.springframework.stereotype.Component;
 public class SysTimeLogAspect {
 
     @Pointcut("@within(com.aaa.mybatisplus.annotation.SysTimeLog) || @annotation(com.aaa.mybatisplus.annotation.SysTimeLog)")
-    public void logPointCut() { }
+    public void logPointCut() {
+    }
 
 
     @Around("logPointCut()")
     public Object doBefore(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         Object proceed = null;
         String beginTime = TimeUtil.getNowDateDetail();
-        log.info( beginTime+" =============》方法 {}开始执行 ......", joinPoint.getSignature().getName());
+        log.info(beginTime + " =============》方法 {}开始执行 ......", joinPoint.getSignature().getName());
         try {
-             proceed = joinPoint.proceed();
+            proceed = joinPoint.proceed();
         } catch (Throwable e) {
             e.printStackTrace();
         }
         String endTime = TimeUtil.getNowDateDetail();
-        log.info( TimeUtil.getNowDateDetail()+" ==================》{}方法 执行结束 ......", joinPoint.getSignature().getName());
-        log.info("共计用时："+TimeUtil.getTimeDifference(beginTime,endTime));
+        log.info(TimeUtil.getNowDateDetail() + " ==================》{}方法 执行结束 ......", joinPoint.getSignature().getName());
+        // log.info("共计用时："+TimeUtil.getTimeDifference(beginTime,endTime));
 
+        stopWatch.stop();
+        log.info("共计用时：{}{}", stopWatch.getTotalTimeSeconds(), "s");
         return proceed;
     }
 }
