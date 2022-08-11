07dc98a6711d110d1bcdb69f2d702f4fbfb955a6
tianliuzhen <tlzallen@163.com>
2022-08-11 21:44:42 +0800

    全局响应Response设置配置http  ResponseHeader

---
+++ src/main/java/com/aaa/mybatisplus/config/AddResponseHeaderFilter.java
@@ -0,0 +1,24 @@
+package com.aaa.mybatisplus.config;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author liuzhen.tian
+ * @version 1.0 AddResponseHeaderFilter.java  2022/8/11 21:39
+ */
+// @Component
+public class AddResponseHeaderFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.addHeader("tlz-version", "1.0.0");
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
