52b9794aabb6d6f4fcc0f886a1c75ff97a4b8b0c
tianliuzhen <tlzallen@163.com>
2023-04-25 22:31:32 +0800

    ThreadMdcUtil 配置异步线程读取traceID

--- src/main/java/com/aaa/mybatisplus/web/TestCrudController.java
+++ src/main/java/com/aaa/mybatisplus/web/TestCrudController.java
@@ -12,6 +12,7 @@ import com.aaa.mybatisplus.domain.enums.ResultCode;
 import com.aaa.mybatisplus.mapper.UserMapper;
 import com.aaa.mybatisplus.service.User2Service;
 import com.aaa.mybatisplus.service.UserService;
+import com.aaa.mybatisplus.util.ThreadMdcUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
@@ -20,6 +21,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,30 +47,31 @@ public class TestCrudController {
     @ApiOperation(value = "分页测试", notes = "插件测试")
     @ApiImplicitParam(name = "pageDto", value = "分页参数", required = true)
     @PostMapping("/testSelectPage")
-    public ResultResponse<?> testSelectPage(@RequestBody PageDto pageDto ) {
-        Page<User> page=new Page();
+    public ResultResponse<?> testSelectPage(@RequestBody PageDto pageDto) {
+        Page<User> page = new Page();
         System.out.println("分页测试：：：");
         // 每页数量、当前页
         page.setSize(pageDto.getSize()).setCurrent(pageDto.getCurrent());
         // 当 total 为小于 0 或者设置 setSearchCount(false) 分页插件不会进行 count 查询
-        IPage<User> iPage=user2Service.selectUserPage(page, "tom");
-        List<User> users=iPage.getRecords();
+        IPage<User> iPage = user2Service.selectUserPage(page, "tom");
+        List<User> users = iPage.getRecords();
         users.forEach(System.out::println);
         log.info("1211");
-        return  ResultResponse.success(page);
+        return ResultResponse.success(page);
     }
+
     /**
-     *简单实现分页，排序、条件筛选
-     {
-     "pageIndex": 1,
-     "pageSize": 1,
-     "orderBy": "id",
-     "direction": "desc",
-     "condition":{"name":"tom1"}
-     }
+     * 简单实现分页，排序、条件筛选
+     * {
+     * "pageIndex": 1,
+     * "pageSize": 1,
+     * "orderBy": "id",
+     * "direction": "desc",
+     * "condition":{"name":"tom1"}
+     * }
      */
     @PostMapping("/testSelectPageV2")
-    public Page<User> testSelectPageV2(@RequestBody @PageAoDefault(orderBy = "createTime") PageAo pageAo ) {
+    public Page<User> testSelectPageV2(@RequestBody @PageAoDefault(orderBy = "createTime") PageAo pageAo) {
 
         // 1. 分页设置
         Page<User> page = pageAo.getPage();
@@ -87,16 +90,18 @@ public class TestCrudController {
         //使用mp自带方法删除和查找都会附带逻辑删除功能 (自己写的xml不会包括注解)
         user2Service.removeById(2);
         user2Service.getById(1);
-        System.out.println(1/0);
+        System.out.println(1 / 0);
         //全表删除或更新
         userMapper.delete(null);
     }
-    @ApiOperation(value = "测试全部删除",notes = "攻击 SQL 阻断解析器")
+
+    @ApiOperation(value = "测试全部删除", notes = "攻击 SQL 阻断解析器")
     @GetMapping("/testDeleteAll")
     public void testDeleteAll() {
         //全表删除或更新
         userMapper.delete(null);
     }
+
     @ApiOperation(value = "测试序列化33", notes = "mybatis-plus通用枚举")
     @PostMapping("/testSelect")
     public void testSelect() {
@@ -105,9 +110,11 @@ public class TestCrudController {
         userList.forEach(System.out::println);
         System.out.println("测试序列化：：：：");
         String jsonString = JSONObject.toJSONString(userList);
-        List<User> list = JSON.parseObject(jsonString, new TypeReference<List<User>>(){});
+        List<User> list = JSON.parseObject(jsonString, new TypeReference<List<User>>() {
+        });
         list.forEach(System.out::println);
     }
+
     @ApiOperation(value = "普通测试", notes = "")
     @GetMapping("/testSelect2")
     public void testSelect2() {
@@ -131,24 +138,25 @@ public class TestCrudController {
     @ApiOperation(value = "批量添加")
     @GetMapping("/testAdds")
     public void testAdds() {
-        List<User> list=new ArrayList<>();
+        List<User> list = new ArrayList<>();
         for (int i = 0; i < 200; i++) {
-            User user=new User();
-            user.setName("Allen"+i);
-            if(i%2==0){
+            User user = new User();
+            user.setName("Allen" + i);
+            if (i % 2 == 0) {
                 user.setSex(GenderEnum.MALE);
-            }else {
+            } else {
                 user.setSex(GenderEnum.FEMALE);
             }
             list.add(user);
         }
-        User user1=new User();
+        User user1 = new User();
         user1.setEmail("74@qq.com").setSex(GenderEnum.FEMALE);
         //测试单个update
         //userService.updateById(user1);
         //测试批量添加
-         userService.saveBatch(list);
+        userService.saveBatch(list);
     }
+
     @ApiOperation(value = "乐观锁测试")
     @GetMapping("/optimisticLocker")
     public void optimisticLocker() {
@@ -158,9 +166,9 @@ public class TestCrudController {
         u.setId("1208249557103063041");
         u.setStatus(version);
         u.setEmail("test@qq.com");
-        if(userService.updateById(u)){
+        if (userService.updateById(u)) {
             System.out.println("Update successfully");
-        }else{
+        } else {
             System.out.println("由于被其他人修改，更新失败(Update failed due to modified by others)");
         }
 
@@ -168,7 +176,7 @@ public class TestCrudController {
 
 
     @ApiOperation(value = "测试多数据源事物")
-    @PostMapping ("/updateBy")
+    @PostMapping("/updateBy")
     // @Transactional(rollbackFor = Exception.class)
     public void update() throws IllegalAccessException {
 
@@ -183,32 +191,44 @@ public class TestCrudController {
         // throw new IllegalAccessException("异常测试");
 
     }
+
     @ApiImplicitParams({
-       @ApiImplicitParam(name="name",value="用户名",dataType="string", paramType = "query",example="allen"),
-       @ApiImplicitParam(name="id",value="用户id",dataType="string", paramType = "query")
+            @ApiImplicitParam(name = "name", value = "用户名", dataType = "string", paramType = "query", example = "allen"),
+            @ApiImplicitParam(name = "id", value = "用户id", dataType = "string", paramType = "query")
     })
     @ApiOperation(value = "测试swagger注解 =》ApiImplicitParams ")
     @PostMapping(value = "testApiImplicitParams")
-    public String testApiImplicitParams(@RequestParam("name") String name,@RequestParam("id") String id){
-        return "name:"+name+"  id"+id;
-    }
-    @ApiOperation(value = "测试swagger注解 =》ApiParam ")
-    @GetMapping("/testApiParam")
-    public String testApiParam( @ApiParam(name="id",value="普通的参数id说明",required=true) @RequestParam("id") String id ){
-        log.info("info:{}",id);
-        log.debug("debug:{}",id);
-        return  "id:"+id;
+    public String testApiImplicitParams(@RequestParam("name") String name, @RequestParam("id") String id) {
+        return "name:" + name + "  id" + id;
     }
 
-    @ApiOperation(value = "测试swagger注解 =》ApiResponses ",notes = "多个参数，多种的查询参数类型")
+    @ApiOperation(value = "测试swagger注解 =》ApiParam ")
+    @GetMapping("/testApiParam")
+    public String testApiParam(@ApiParam(name = "id", value = "普通的参数id说明", required = true) @RequestParam("id") String id) {
+        log.info("info:{}", id);
+        log.debug("debug:{}", id);
+
+        new Thread(() -> {
+            log.error("info2:{}", id);
+        }).start();
+        new Thread(ThreadMdcUtil.wrap(()->{
+            log.error("info3:{}", id);
+        }, MDC.getCopyOfContextMap())).start();
+
+
+
+        return "id:" + id;
+    }
+
+    @ApiOperation(value = "测试swagger注解 =》ApiResponses ", notes = "多个参数，多种的查询参数类型")
     @ApiResponses({
-            @ApiResponse(code=400,message="请求参数没填好"),
-            @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对"),
-            @ApiResponse(code=500,message="服务器异常")
+            @ApiResponse(code = 400, message = "请求参数没填好"),
+            @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对"),
+            @ApiResponse(code = 500, message = "服务器异常")
     })
     @GetMapping("/testApiResponses")
-    public void testApiResponses(@RequestParam("id") int id){
-        if(id>10){
+    public void testApiResponses(@RequestParam("id") int id) {
+        if (id > 10) {
             Shift.fatal(ResultCode.SYSTEM_ERROR);
         }
     }
