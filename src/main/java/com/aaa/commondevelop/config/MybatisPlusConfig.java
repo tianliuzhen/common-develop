68359f7a8dc373e0d3c09a2f0dc5db07b0cefeaa
tianliuzhen <tlzallen@163.com>
2025-07-06 21:59:24 +0800

    @DS("slave") 默认是 @DS("master")

--- src/main/java/com/aaa/commondevelop/config/MybatisPlusConfig.java
+++ src/main/java/com/aaa/commondevelop/config/MybatisPlusConfig.java
@@ -16,7 +16,18 @@ import org.springframework.context.annotation.Configuration;
 // @MapperScan("com.aaa.commondevelop.mapper*")
 @Slf4j
 public class MybatisPlusConfig {
-    int a=1 ;
+    // @Bean
+    // public PageInterceptor pageInterceptor() {
+    //     PageInterceptor pageInterceptor = new PageInterceptor();
+    //     Properties properties = new Properties();
+    //     properties.setProperty("helperDialect", "mysql");
+    //     properties.setProperty("reasonable", "true");
+    //     properties.setProperty("supportMethodsArguments", "true");
+    //     properties.setProperty("params", "count=countSql");
+    //     properties.setProperty("pageSizeZero", "true");
+    //     pageInterceptor.setProperties(properties);
+    //     return pageInterceptor;
+    // }
 
   /*  @Bean
     public PaginationInterceptor paginationInterceptor() {
--- src/main/java/com/aaa/commondevelop/mappertk/EmpTkMapper.java
+++ src/main/java/com/aaa/commondevelop/mappertk/EmpTkMapper.java
@@ -2,8 +2,10 @@ package com.aaa.commondevelop.mappertk;
 
 import com.aaa.commondevelop.domain.entity.Emp;
 import com.aaa.commondevelop.mappertk.common.BatchMapper;
+import com.baomidou.dynamic.datasource.annotation.DS;
 import tk.mybatis.mapper.common.Mapper;
 
+@DS("slave")
 public interface EmpTkMapper extends Mapper<Emp>, BatchMapper<Emp> {
 
 }
